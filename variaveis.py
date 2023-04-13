4bebd3457ba914a7a35232a303366422575f7355
crsouza993 <crsouza993@gmail.com>
2023-04-13 18:50:28 -0300

    tipos de variaveis em python

--- variaveis.py
+++ variaveis.py
@@ -2,3 +2,30 @@
 
 velocidade_maxima = 240
 print(velocidade_maxima)
+
+frase_de_boas_vindas = "ola mundo"   string em python
+print(frase_de_boas_vindas)
+
+operadores matematicos
+true or false 
+
+posso_entrar = false #boleanos
+
+#valores decimais
+
+dollar = 5.25 #Decimal
+print(type(velocidade_maxima))
+print(type(dollar))
+print(type(frase_boas_vindas))
+
+#descobrindo o tipo da váriavel sem ter mencionado o tipo dela  '' (type(variavel)) 
+#type tras o tipo da variavel 
+print(type(frase_boas_vindas))
+
+#atribuir em uma unica linha. Passando o valor em ordem
+
+#atribuição mutiplas
+velocidade, frase, posso_ou_nao = 100, 'ola', True
+print(velocidade)
+print(frase)
+print(posso_ou_não)
