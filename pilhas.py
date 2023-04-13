1ab65b1391abcf0842201336359845368f1377a0
crsouza993 <crsouza993@gmail.com>
2023-04-13 16:55:52 -0300

    exercicio básico de pilhas em python

--- pilhas.py
+++ pilhas.py
@@ -11,4 +11,12 @@ self.next = None
 
 class stack:
 
-    def __init
+    def __init__(self);
+    self.top = None
+    self.size = 0
+
+    def push(self,elem):
+        node = Node(elem)
+        node.next = self.top
+        self.top = node
+        self.size += 1
