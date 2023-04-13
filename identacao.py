8776336a7803887ce652ed2298be2157c0c2bd19
crsouza993 <crsouza993@gmail.com>
2023-04-13 19:07:17 -0300

    dando continuidade

---
+++ identacao.py
@@ -0,0 +1,20 @@
+
+
+
+
+import time 
+
+#método 'pensarporsegundos'
+
+def PensarPor10Segundos(): 
+    print('pensando')
+    time.sleep(10)
+    print('lembrei!')
+
+
+
+if 10 > 5:
+	print('10 é maior que 5')
+	if 'hello' == 'hello':
+			print('hello!')
+            
