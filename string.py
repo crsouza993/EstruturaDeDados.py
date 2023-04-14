061ace20350b2b1ed28238e672c731905d1fca74
crsouza993 <crsouza993@gmail.com>
2023-04-13 21:16:34 -0300

    strings e variaveis

---
+++ string.py
@@ -0,0 +1,21 @@
+#strings 
+
+nome_do_instrumento = 'guitarra eletrica'
+nome_do_instrumento = "guitarra eletrica"
+print(nome_do_instrumento)
+
+#Caracteres de escape  "o filme foi demais"
+
+resenha_do_filme = "o filme foi demais"
+resenha_do_filme2 = "o filme \"foi demais\""    #escape = deixando o valor entre aspas ao imprimir
+resenha_do_filme3 = "o filme foi \'demais\'"
+resenha_do_filme4 = 'o filme foi \ndemais'
+minhas_fotos = 'meu computador\\fotos\\minhas fotos'
+
+print(resenha_do_filme)
+print(resenha_do_filme2)
+print(resenha_do_filme3)
+print(resenha_do_filme4)
+print(minhas_fotos)
+
+
--- variaveis.py
+++ variaveis.py
@@ -3,29 +3,27 @@
 velocidade_maxima = 240
 print(velocidade_maxima)
 
-frase_de_boas_vindas = "ola mundo"   string em python
+frase_de_boas_vindas = "ola mundo"   #string em python
 print(frase_de_boas_vindas)
 
-operadores matematicos
-true or false 
+#operadores matematicos
+#true or false 
 
-posso_entrar = false #boleanos
+posso_entrar = False 
+
+#boleanos
 
 #valores decimais
 
 dollar = 5.25 #Decimal
 print(type(velocidade_maxima))
 print(type(dollar))
-print(type(frase_boas_vindas))
+print(type(frase_de_boas_vindas))
 
 #descobrindo o tipo da váriavel sem ter mencionado o tipo dela  '' (type(variavel)) 
 #type tras o tipo da variavel 
-print(type(frase_boas_vindas))
+print(type(frase_de_boas_vindas))
 
 #atribuir em uma unica linha. Passando o valor em ordem
 
-#atribuição mutiplas
-velocidade, frase, posso_ou_nao = 100, 'ola', True
-print(velocidade)
-print(frase)
-print(posso_ou_não)
+
